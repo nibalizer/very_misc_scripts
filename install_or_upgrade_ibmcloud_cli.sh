fa7af177fc83a6258807fe30e7916d6662233feb
Spencer Krum <nibz@spencerkrum.com>
2018-09-24 17:29:05 -0500

    Add UNSUPPORTED ibmcloud cli install script

---
+++ install_or_upgrade_ibmcloud_cli.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "This script will install or upgrade your ibmcloud cli utility"
+echo "This script has NO WARRANTY and is UNSUPPORTED"
+
+temp_dir=$(mktemp -d)
+linux_url="https://clis.ng.bluemix.net/download/bluemix-cli/latest/linux64"
+
+cd $temp_dir
+wget $linux_url
+mv linux64 linux64.tar.gz 
+tar -xvzf linux64.tar.gz
+cd Bluemix_CLI/
+
+cp -f "bin/ibmcloud" "${HOME}/local/bin/"
+cp -f "bin/ibmcloud-analytics" "${HOME}/local/bin/"
+cp -f "autocomplete/bash_autocomplete" "${HOME}/.bash_completion.d/ibmcloud_autocomplete"
+
+cd /tmp
+rm -fr "$temp_dir"
+
+
