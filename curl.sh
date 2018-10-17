42ec62dd76f415b8b2422948df4d205fba73d1e8
Spencer Krum <nibz@spencerkrum.com>
2018-10-17 16:18:11 -0500

    Add curl.sh

--- curl.sh
+++ curl.sh
@@ -4,3 +4,9 @@ exit
 # JSON mode
 curl -H "Accept: application/json" -H "Content-Type: application/json"
 
+
+# Other Tools
+
+wrk - https://github.com/wg/wrk - like ab but better
+
+httpie - https://httpie.org/ - maybe better than curl?
