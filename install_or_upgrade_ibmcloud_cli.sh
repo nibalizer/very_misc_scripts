205a499db69b16ea8bf0681ba8a4a7d23b9f435e
Spencer Krum <nibz@spencerkrum.com>
2019-02-24 10:31:48 -0600

    Add container registry to plugins list for ibmcloud

--- install_or_upgrade_ibmcloud_cli.sh
+++ install_or_upgrade_ibmcloud_cli.sh
@@ -24,6 +24,7 @@ rm -fr "$temp_dir"
 ${HOME}/local/bin/ibmcloud config --usage-stats-collect false
 ${HOME}/local/bin/ibmcloud plugin install -f kubernetes-service
 ${HOME}/local/bin/ibmcloud plugin install -f cloud-functions
+${HOME}/local/bin/ibmcloud plugin install -f container-registry
 
 echo "ibmcloud cli utility installed/upgraded to ${HOME}/local/bin"
 echo "add it to your path to start using it"
